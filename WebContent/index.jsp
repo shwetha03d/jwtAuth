da6e25ce7d2bf9cb55aed76f8b10b336f18bb7e9
shwetha03d <50769122+shwetha03d@users.noreply.github.com>
2020-03-09 16:03:38 +0530

    Add files via upload

---
+++ WebContent/index.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Spring Rest JWT Authentication</title>
+</head>
+<body>
+	<!-- UserName:<input type="text" name="username" id="uName"><br>
+	Password:<input type="text" name="password" id="pass"><br>
+	
+	<button type="submit" value="submit">AddUser</button>
+	
+ -->
+</body>
+</html>
---
+++ src/com/javachinna/config/WebConfig.java
@@ -0,0 +1,143 @@
+package com.javachinna.config;
+
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.validation.Validator;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan("com.javachinna")
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
+@PropertySource(value = { "classpath:application.properties" })
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(new MappingJackson2HttpMessageConverter());
+	}
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addFormatters(FormatterRegistry arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addReturnValueHandlers(List<HandlerMethodReturnValueHandler> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configureAsyncSupport(AsyncSupportConfigurer arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configurePathMatch(PathMatchConfigurer arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void extendMessageConverters(List<HttpMessageConverter<?>> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public MessageCodesResolver getMessageCodesResolver() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Validator getValidator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
